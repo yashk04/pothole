1e9b98a11517b6205471ef489ab5f6c9163285b2
shubh <shubhamfargade99@gmail.com>
2020-01-09 10:13:19 +0530

    upload or upvote half

--- app/src/main/java/com/example/pothole/Citizen/image/ImageFragment.java
+++ app/src/main/java/com/example/pothole/Citizen/image/ImageFragment.java
@@ -2,14 +2,17 @@ package com.example.pothole.Citizen.image;
 
 import android.Manifest;
 import android.app.AlertDialog;
+import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.location.Location;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,6 +23,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
@@ -30,8 +34,13 @@ import com.example.pothole.R;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.MutableData;
+import com.google.firebase.database.Transaction;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
@@ -44,6 +53,7 @@ import static android.app.Activity.RESULT_OK;
 
 public class ImageFragment extends Fragment {
     FirebaseAuth mAuth;
+    int flagforterminate = 0;
     private StorageReference mStorageRef;
     public static final int REQUESTCODE = 999;
     public static final int RequestPermissionCode = 1;
@@ -60,9 +70,17 @@ public class ImageFragment extends Fragment {
     LocationTrack locationTrack;
     double latitude;
     DatabaseReference databaseReference;
+    String imageUrl,tempPotholeUid;
+    ProgressDialog progressDialog;
+    int flagForUpvoteOrUpload;
+    int temppoints;
+    Upload_Upvote obj;
+    Potholesnearmy pnm;
+    boolean flag;
+    Upload_Upvote objUpvote,objUpload;
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         imageViewModel = ViewModelProviders.of(this).get(ImageViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_image, container, false);
+       final View root = inflater.inflate(R.layout.fragment_image, container, false);
         final TextView textView = root.findViewById(R.id.text_home);
         /*imageViewModel.getText().observe(this, new Observer<String>() {
             @Override
@@ -70,6 +88,10 @@ public class ImageFragment extends Fragment {
                 textView.setText(s);
             }
         });*/
+
+        progressDialog = new ProgressDialog(getContext());
+        progressDialog.setMessage("Processing...");
+
         btntakephoto = (Button) root.findViewById(R.id.btnimage);
         btnsubmit = (Button) root.findViewById(R.id.btnsubmit);
         content = (LinearLayout) root.findViewById(R.id.content);
@@ -130,49 +152,114 @@ public class ImageFragment extends Fragment {
                     bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream);
                     byte[] byteArray = stream.toByteArray();
 
-                    //Connectivity c = new Connectivity(getContext());
-                    //c.execute(byteArray);
+                    //##########ANAND'S CODE FOR DECIDING UPVOTE OR UPLOAD....##########
+                    //decide flagForUpvoteOrUpload ..1 if upload and 0 if upvote
 
-                    //for adding pothole data into pothole node
-                    databaseReference = FirebaseDatabase.getInstance().getReference().child("pothole");
-                    String tempPotholeUid = databaseReference.push().getKey();
+                   //obj=new Upload_Upvote();
 
-                    PotholeLocation pht=new PotholeLocation(latitude,longitude,1,1,FirebaseAuth.getInstance().getUid(),tempPotholeUid);
+                   // pnm = new Potholesnearmy(latitude,longitude,getContext());
+                   // obj=pnm.getPothole();
+                  //  progressDialog.show();
+                    obj = getPothole();
 
-                    databaseReference.child(tempPotholeUid).setValue(pht);
-                    //#######adding pothole information done here##################
+                    Log.i("shubhamanandLat",obj.getLati()+"");
+                    Log.i("shubhamanandLong",obj.getLongi()+"");
 
-                    //######for adding pothole image in firebase storage###########
-                    mAuth = FirebaseAuth.getInstance();
-                    String uid=mAuth.getUid();
-                    String path="Pothole_photos/"+tempPotholeUid+"/mypothole.png";
-                    mStorageRef= FirebaseStorage.getInstance().getReference(path);
+                    if(obj.getLati()!=0.0 && obj.getLongi()!=0.0)
+                    {
+                        //Upvoted;
+                        Toast.makeText(getContext(),"Pothole present already,so upvoted",Toast.LENGTH_LONG).show();
+                        Log.i("shubham","Pothole upvoted");
+                        String potholeid=obj.getPotholeid();
 
-                    UploadTask uploadTask = mStorageRef.putBytes(byteArray);
-                    uploadTask.addOnFailureListener(new OnFailureListener() {
-                        @Override
-                        public void onFailure(@NonNull Exception exception) {
-                            // Handle unsuccessful uploads
-                            Toast.makeText(getContext(),"fail to upload",Toast.LENGTH_SHORT);
+                        databaseReference = FirebaseDatabase.getInstance().getReference().child("pothole").child(potholeid).child("noOfUpvotes");
 
-                        }
-                    }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-                        @Override
-                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                            Toast.makeText(getContext(),"Successful to upload",Toast.LENGTH_SHORT);
-                        }
-                    });
-                    //############adding image done here################3
+                        databaseReference.runTransaction(new Transaction.Handler() {
+                            @NonNull
+                            @Override
+                            public Transaction.Result doTransaction(@NonNull MutableData mutableData) {
+                                int tempUpvotes = mutableData.getValue(Integer.class);
+                                mutableData.setValue(tempUpvotes + 1);
+                                return Transaction.success(mutableData);
+                            }
+                            @Override
+                            public void onComplete(@Nullable DatabaseError databaseError, boolean b, @Nullable DataSnapshot dataSnapshot) {
+                                Log.i("shubham", "increment error");
+                            }
+                        });
+                        databaseReference = FirebaseDatabase.getInstance().getReference().child("user").child(mAuth.getUid()).child("upvotedPothole");
+                        databaseReference.child(potholeid).setValue(1);
+                    }
+                    else {
+                        //Connectivity c = new Connectivity(getContext());
+                        //c.execute(byteArray);
 
-                    //#######3adding this info to user's node##############
-                    databaseReference = FirebaseDatabase.getInstance().getReference().child(FirebaseAuth.getInstance().getUid());
+                        //######for adding pothole image in firebase storage###########
+                        Toast.makeText(getContext(),"New Pothole detected, so uploaded",Toast.LENGTH_LONG).show();
+                        Log.i("shubham","Pothole uploaded");
+
+                        mAuth = FirebaseAuth.getInstance();
+                        String uid = mAuth.getUid();
+                        String path = "Pothole_photos/" + System.currentTimeMillis() + ".jpg";
+                        mStorageRef = FirebaseStorage.getInstance().getReference(path);
+
+                        final UploadTask uploadTask = mStorageRef.putBytes(byteArray);
+                        uploadTask.addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception exception) {
+                                // Handle unsuccessful uploads
+                                Toast.makeText(getContext(), "fail to upload", Toast.LENGTH_SHORT);
+
+                            }
+                        }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                            @Override
+                            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                                Toast.makeText(getContext(), "Successful to upload", Toast.LENGTH_SHORT);
+                                taskSnapshot.getMetadata().getReference().getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                    @Override
+                                    public void onSuccess(Uri uri) {
+                                        imageUrl = uri.toString();
+
+                                        //#########for adding pothole data into pothole node###########
+                                        databaseReference = FirebaseDatabase.getInstance().getReference().child("pothole");
+                                        tempPotholeUid = databaseReference.push().getKey();
+
+                                        PotholeLocation pht = new PotholeLocation(latitude, longitude, 1, 1, FirebaseAuth.getInstance().getUid(), tempPotholeUid, imageUrl);
+                                        databaseReference.child(tempPotholeUid).setValue(pht);
+
+                                        databaseReference = FirebaseDatabase.getInstance().getReference().child("user").child(FirebaseAuth.getInstance().getUid());
+                                        databaseReference.child("uploadedPothole").child(tempPotholeUid).setValue(1);
+
+                                            databaseReference = FirebaseDatabase.getInstance().getReference().child("user").child(FirebaseAuth.getInstance().getUid()).child("points");
+                                            databaseReference.runTransaction(new Transaction.Handler() {
+                                                @NonNull
+                                                @Override
+                                                public Transaction.Result doTransaction(@NonNull MutableData mutableData) {
+                                                    temppoints = mutableData.getValue(Integer.class);
+                                                    mutableData.setValue(temppoints + 10);
+                                                    return Transaction.success(mutableData);
+                                                }
+
+                                                @Override
+                                                public void onComplete(@Nullable DatabaseError databaseError, boolean b, @Nullable DataSnapshot dataSnapshot) {
+                                                    Log.i("shubham", "increment error");
+                                                }
+                                            });
+                                    }
+                                });
+                            }
+                        });
+                        progressDialog.dismiss();
+                    }
                 } catch (Exception e) {
                     e.printStackTrace();
                     Toast.makeText(getActivity(), "Server Down! :(", Toast.LENGTH_SHORT).show();
+                    progressDialog.dismiss();
                 }
+
             }
         });
-        return root;
+  return root;
     }
 
     private void requestGPSPermission() {
@@ -194,12 +281,8 @@ public class ImageFragment extends Fragment {
                     }).create().show();
         } else {
             ActivityCompat.requestPermissions(this.getActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, RequestPermissionCodeGPS);
-
         }
-
     }
-
-
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -219,24 +302,15 @@ public class ImageFragment extends Fragment {
             }
         }
     }
-
     public void EnableRuntimePermission() {
-
         if (ActivityCompat.shouldShowRequestPermissionRationale(Objects.requireNonNull(this.getActivity()),
                 Manifest.permission.CAMERA)) {
-
             Toast.makeText(this.getActivity(), "CAMERA permission allows us to Access CAMERA app", Toast.LENGTH_LONG).show();
-
         } else {
-
             ActivityCompat.requestPermissions(this.getActivity(), new String[]{
-
                     Manifest.permission.CAMERA}, RequestPermissionCode);
-
         }
-
     }
-
     @Override
     public void onRequestPermissionsResult(int RC, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(RC, permissions, grantResults);
@@ -261,6 +335,83 @@ public class ImageFragment extends Fragment {
                     Toast.makeText(this.getActivity(), "Permission Canceled", Toast.LENGTH_LONG).show();
                 }
         }
-
     }
+
+    public Upload_Upvote getPothole() {
+        objUpvote=new Upload_Upvote();
+        objUpload=new Upload_Upvote();
+
+       FirebaseDatabase firebaseDatabase =  FirebaseDatabase.getInstance();
+       DatabaseReference mDatabase = firebaseDatabase.getReference("pothole");
+//        progressDialog1 = new ProgressDialog(mycontext);
+//        progressDialog1.setMessage("In Potholes near me class");
+        Log.i("shubhamoriginallat",latitude+"");
+        Log.i("shubhamoriginallon",longitude+"");
+
+        mDatabase.addValueEventListener( new ValueEventListener(){
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                flag=false;
+                progressDialog.show();
+                //an arraylist to save all the latitude and longitude values
+                if(flagforterminate!=1) {
+                    for (DataSnapshot post : dataSnapshot.getChildren()) {
+                        // Iterate through all your users and get latitude and longitudea
+                        // if(post.child("lat").getValue(double.class)!=null && post.child("longi").getValue(double.class)!=null )
+                        // {
+                        double latitud = post.child("lat").getValue(double.class);
+                        double longitud = post.child("longi").getValue(double.class);
+                        String potholeid = post.child("potholeUid").getValue(String.class);
+                        Log.i("shubhampotholeid", potholeid);
+                        Log.i("shubhamlatitude", latitud + "");
+                        Log.i("shubhamlongitu", longitud + "");
+
+                        // Toast.makeText(getApplicationContext(), Double.toString(longitud)+"Hi", Toast.LENGTH_LONG).show();
+                        //store your latlang
+
+                        if (distance(latitude, longitude, latitud, longitud) <= 50.0)//checking distance
+                        {
+                            // Toast.makeText(getApplicationContext(), Double.toString(latitud)+"hello", Toast.LENGTH_SHORT).show();
+                            //go for upvote
+                            Log.i("shubham", "inUpvote");
+                            flag = true;
+                            objUpvote.setLati(latitud);
+                            objUpvote.setLongi(longitud);
+                            objUpvote.setPotholeid(potholeid);
+                            flagforterminate = 1;
+                            return;
+                        } else {
+                            //go for upload
+                            Log.i("shubham", "inUpload");
+                            Toast.makeText(getContext(), "mark lat and  map", Toast.LENGTH_LONG).show();
+                            flag = false;
+                            objUpload.setLongi(0.0);
+                            objUpload.setLati(0.0);
+                            objUpload.setPotholeid(potholeid);
+                        }
+                        //}
+                    }
+                }
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {}
+        });
+        if(flag==false)//return upload object
+        {
+            return objUpload;
+        }
+        else//return upvote object
+        {
+            return objUpvote;
+        }
+    }
+
+    public float distance(double currentlatitude, double currentlongitude, double originLat, double originLon) {
+
+        float[] results = new float[1];
+        Location.distanceBetween(currentlatitude, currentlongitude, originLat, originLon, results);
+        float distanceInMeters = results[0];
+        //  Toast.makeText(getApplicationContext(),Float.toString(distanceInMeters)+"tu",Toast.LENGTH_LONG).show();
+        return distanceInMeters;
+    }
+
 }
