83fa6098a85ee039681b253ac02c0c4fd1cbf98e
yashk04 <48150312+yashk04@users.noreply.github.com>
2020-01-11 11:57:39 +0530

    Add files via upload

---
+++ html/index1.js
@@ -0,0 +1,329 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBKSRuQIWn9SsOyfMkaLi5xJVo2UU__ijw",
+    authDomain: "pothole-b8122.firebaseapp.com",
+    databaseURL: "https://pothole-b8122.firebaseio.com",
+    projectId: "pothole-b8122",
+    storageBucket: "pothole-b8122.appspot.com",
+    messagingSenderId: "543188174392",
+    appId: "1:543188174392:web:a383b8098730b3f519c8cd",
+    measurementId: "G-7822SGDGKJ"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+function initMap()
+{
+    var locations = [];
+    var potholeImages = [];
+    var ref = firebase.database().ref('pothole');
+    var options = {
+      zoom: 17,
+      center: {lat: 18.4575,lng:73.8508}
+    }
+
+    var map = new google.maps.Map(document.getElementById('map'), options);
+    ref.on('child_added',function(snapshot) {
+
+      var latitude = snapshot.val();
+
+      if(latitude.statusFlag == 0) {
+        //pothole work not assigned
+        var a = latitude.potholeCount;
+        var marker = new google.maps.Marker({
+              icon: new google.maps.MarkerImage("http://maps.google.com/mapfiles/ms/icons/red-dot.png"),
+              position:{lat: latitude.lat, lng: latitude.longi},
+              map:map,
+              url: "",
+              title: ""+a
+            });
+      }
+      else if(latitude.statusFlag == 1) {
+        //pothole work is assigned but not started
+        var b = latitude.potholeCount;
+        var marker = new google.maps.Marker({
+              icon: new google.maps.MarkerImage("http://maps.google.com/mapfiles/ms/icons/yellow-dot.png"),
+              position:{lat: latitude.lat, lng: latitude.longi},
+              map:map,
+              url: "",
+              title: ""+b
+            });
+      }
+      else if(latitude.statusFlag == 2) {
+        //pothole work in progress
+        var c = latitude.potholeCount;
+        var marker = new google.maps.Marker({
+              icon: new google.maps.MarkerImage("http://maps.google.com/mapfiles/ms/icons/blue-dot.png"),
+              position:{lat: latitude.lat, lng: latitude.longi},
+              map:map,
+              url: "",
+              title: ""+c
+            });
+      }
+      else if(latitude.statusFlag == 3) {
+        //pothole work completed
+        var d = latitude.potholeCount;
+
+        var marker = new google.maps.Marker({
+              icon: new google.maps.MarkerImage("http://maps.google.com/mapfiles/ms/icons/green-dot.png"),
+              position:{lat: latitude.lat, lng: latitude.longi},
+              map:map,
+              url: "",
+              title: ""+d
+            });
+
+        $("#menu4").append("<li>" + snapshot.key + "<button class='finishButton' id='" + snapshot.key +"'>Finish</button>" + "</li>");
+        $("#menu4").append("<div class='row'><div class='col'><ul><li>" + snapshot.key + "</li></ul></div>" + "<div class='col'><button class='hireButton btn btn-md btn-success float-right' id='" + snapshot.key +"'>Hire</button></div></div><br>");
+        $("button.finishButton").click(function() {
+                firebase.database().ref('pothole/' + snapshot.key +"/").update({
+                  statusFlag: 4,
+                  noOfUpvotes: 0
+                }, function(error) {
+                  if (error) {
+                    // The write failed...
+                    alert(error);
+                  } else {
+                    // Data saved successfully!
+                    alert("Data saved");
+                  }
+                });
+
+                if(snapshot.key === this.id) {
+
+                  firebase.database().ref('contractor/' + snapshot.val().contractorUid +"/").update({
+                    busyFlag: 0
+
+                  }, function(error) {
+                    if (error) {
+                      // The write failed...
+                      alert(error);
+                    } else {
+                      // Data saved successfully!
+                      alert("Data saved");
+                    }
+                  });
+                  firebase.database().ref('contractor/' + snapshot.val().contractorUid +"/allotedPothole").remove().then(function() {
+                    alert("Removed succed");
+                  })
+                  .catch(function(error) {
+                    alert(error.message);
+                  });
+                }
+
+                window.location.reload();
+            });
+      }
+
+
+//click started
+google.maps.event.addListener(marker, 'click', function(e) {
+
+     // document.getElementById('lati').innerHTML = e.latLng.lat();
+     // document.getElementById('long').innerHTML = e.latLng.lng();
+     // var flag = 0;
+     ref.orderByChild('lat').equalTo(e.latLng.lat()).on('child_added', function(snapshot) {
+       var value = snapshot.val();
+       if(!locations.includes(value.potholeUid) && value.statusFlag == 0) {
+         locations.push(value.potholeUid);
+         potholeImages.push(value.potholeImageUid);
+         alert(locations);
+       }else if(locations.includes(value.potholeUid)){
+         locations.splice(locations.indexOf(value.potholeUid), 1);
+         potholeImages.splice(potholeImages.indexOf(value.potholeImageUid), 1);
+         alert(locations);
+       }
+       // if(flag == 0) {
+       var marker = new google.maps.Marker({
+             icon: new google.maps.MarkerImage("http://maps.google.com/mapfiles/ms/icons/purple-dot.png"),
+             position:{lat: e.latLng.lat(), lng: e.latLng.lng()},
+             map:map,
+             url: "",
+             title: snapshot.val().potholeCount
+       });
+       // flag = 1;
+       // }else if(flag == 1) {
+       //   var marker = new google.maps.Marker({
+       //         icon: new google.maps.MarkerImage("http://maps.google.com/mapfiles/ms/icons/red-dot.png"),
+       //         position:{lat: e.latLng.lat(), lng: e.latLng.lng()},
+       //         map:map,
+       //         url: "",
+       //         title: snapshot.val().potholeCount
+       //       });
+       //       flag = 0;
+       // }
+       //EMail of contractor displayed
+       //alert(value.contractorUid);
+       //var ref5 = firebase.database().ref('contractor');
+       //ref5.orderByChild('email').equalTo(value.contractorUid).on('child_added',function(snapshot1) {
+         // alert("on contractor");
+         // if(value.contractorUid === snapshot1.key)
+         // {
+         //   document.getElementById('contractorEmail').innerHTML = snapshot1.val().email;
+         // }
+         // else {
+         //   document.getElementById('contractorEmail').innerHTML = " ";
+         // }
+        // alert("yes");
+         //document.getElementById('contractorEmail').innerHTML = snapshot1.;
+      //});
+       document.getElementById('image').src = value.potholeImageUid;
+       document.getElementById('location').innerHTML = value.fullAddress;
+       document.getElementById('potholes').innerHTML = value.potholeCount;
+       document.getElementById('upvotes').innerHTML = value.noOfUpvotes;
+       document.getElementById('contractorEmail').innerHTML = value.contractorEmail;
+
+       if(value.statusFlag == 0)
+       {
+         document.getElementById('work').innerHTML = "Work not started";
+       }
+       else if(value.statusFlag == 1)
+       {
+         document.getElementById('work').innerHTML = "Work is assigned";
+
+       }
+       else if(value.statusFlag == 2)
+       {
+         document.getElementById('work').innerHTML = "Work is in progress";
+       }
+       else if(value.statusFlag == 3)
+       {
+         document.getElementById('work').innerHTML = "Work is finished";
+       }
+       // snapshot.forEach(function(data) {
+       //     console.log(data.key);
+
+       // });4
+
+     });
+
+   });
+
+///click end
+});//ref for pothole marking ends here
+
+
+//displaying emails starts here
+            firebase.database().ref('contractor').on('child_added',function(snapshot1) {
+              // if(value.contractorUid === snapshot1.key)
+              // {
+              //   document.getElementById('contractorEmail').innerHTML = snapshot1.val().email;
+              // }
+
+            var id = snapshot1.key;
+            var emailId = snapshot1.val().email;
+            // if(value.statusFlag == 0) {
+
+              if(snapshot1.val().busyFlag == 0) {
+                $("#home").append("<div class='row'><div class='col'><ul><li>" + emailId + "</li></ul></div>" + "<div class='col'><button class='hireButton btn btn-md btn-success float-right' id='" + id +"'>Hire</button></div></div><br>");
+              }
+              else if(snapshot1.val().busyFlag == 1) {
+                $("#menu1").append("<div class='row'><div class='col'><ul><li>" + emailId + "</li></ul></div>" + "<br>");
+              }
+              else if(snapshot1.val().busyFlag == 2) {
+                $("#menu2").append("<div class='row'><div class='col'><ul><li>" + emailId + "</li></ul></div>" + "<div class='col'><button class='removeButton btn btn-md btn-danger float-right' id='" + id +"'>Remove</button></div></div><br>");
+              }
+              if(snapshot1.val().busyFlag != 2) {
+                $("#menu3").append("<div class='row'><div class='col'><ul><li>" + emailId + "</li></ul></div>" + "<div class='col'><button class='blackListButton btn btn-md btn-success float-right' id='" + id +"'>Add</button></div></div><br>");
+              }
+
+              $("button.hireButton").click(function() {
+                var i;
+                for(i=0;i < locations.length;i++) {
+                  var email;
+                  if(id === this.id) {
+                    email = snapshot1.val().email;
+                  }
+                  // alert(email);
+                  firebase.database().ref('pothole/' + locations[i] +"/").update({
+                    contractorUid: this.id,
+                    statusFlag: 1,
+                    contractorEmail: email
+                  }, function(error) {
+                    if (error) {
+                      // The write failed...
+                      alert(error.message);
+                    } else {
+                      // Data saved successfully!
+                      alert("Data saved");
+                    }
+                  });
+
+                  if(id === this.id) {
+                    firebase.database().ref('contractor/' + id +"/allotedPothole/"+ locations[i] + "/").update({
+                      potholeImageUid: potholeImages[i]
+
+                    }, function(error) {
+                      if (error) {
+                        // The write failed...
+                        alert(error.message);
+                      } else {
+                        // Data saved successfully!
+                        alert("Data saved");
+                      }
+                    });
+
+                    firebase.database().ref('contractor/' + id +"/").update({
+                      busyFlag: 1
+
+                    }, function(error) {
+                      if (error) {
+                        // The write failed...
+                        alert(error.message);
+                      } else {
+                        // Data saved successfully!
+                        alert("Data saved");
+                      }
+                    });
+                  }
+
+
+                }
+                window.location.reload();
+              });
+
+
+              $("button.blackListButton").click(function() {
+
+                  if(id === this.id) {
+
+                    firebase.database().ref('contractor/' + id +"/").update({
+                      busyFlag: 2
+
+                    }, function(error) {
+                      if (error) {
+                        // The write failed...
+                        alert(error);
+                      } else {
+                        // Data saved successfully!
+                        alert("Data saved");
+                      }
+                    });
+                  }
+
+                  window.location.reload();
+              });
+
+              $("button.removeButton").click(function() {
+
+                  if(id === this.id) {
+
+                    firebase.database().ref('contractor/' + id +"/").update({
+                      busyFlag: 0
+
+                    }, function(error) {
+                      if (error) {
+                        // The write failed...
+                        alert(error);
+                      } else {
+                        // Data saved successfully!
+                        alert("Data saved");
+                      }
+                    });
+                  }
+
+                  window.location.reload();
+              });
+
+      });
+}
+google.maps.event.addDomListener(window, 'load', initMap);
